3ffa956b1a1294a9f2a93bf14c32d8b66f83cfc1
Ronnie Rodriguez <rlrodrig88@gmail>
2019-06-29 19:16:45 -0500

    fixed css layout for slider, added valitation and error message

--- app.js
+++ app.js
@@ -2,11 +2,20 @@
 const slider = document.querySelector("#slider");
 
 const age = document.querySelector("#ageInput");
-let formData = new FormData(myForm);
+const ageError = document.querySelector("#ageError");
 
 myForm.addEventListener("submit", event => {
-  event.preventDefault();  
-  console.log(age.value);
+  event.preventDefault();
+  if (validate(age.value) === false) return; 
   slider.style = "width: " + age.value + "%";
 })
 
+let validate = function(value) {
+  if (age.value <= 100 && age.value >= 0) {
+    ageError.innerHTML = "";
+    return true;
+  } else {
+    ageError.innerHTML = "Please enter an age between 0 and 100";
+    return false;
+  }
+}
